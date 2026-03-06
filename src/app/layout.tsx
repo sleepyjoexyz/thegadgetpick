d4939ac59106d169e2208f0defc0408afd19e270
dwuai <dwu@dwuconsulting.com>
2026-03-06 07:25:40 -0800

    fix:JSON-LD-move-WebSiteSchema-to-body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,9 +70,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <WebSiteSchema />
       </head>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
+        <WebSiteSchema />
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-blue-600 focus:text-white focus:rounded"
