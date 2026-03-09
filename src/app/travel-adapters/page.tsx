b814d95065fa710e19030071d87a98dd7d8f0e52
PE <pelosi.lee9527@gmail.com>
2026-03-08 19:29:46 -0700

    Fix JSX nesting: remove stray </p></section> in turntables and travel-adapters

--- src/app/travel-adapters/page.tsx
+++ src/app/travel-adapters/page.tsx
@@ -191,8 +191,6 @@ export default function TravelAdaptersContent() {
           }}
         />
       </section>
-        </p>
-      </section>
 
       {/* Filters */}
       <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
--- src/app/turntables/page.tsx
+++ src/app/turntables/page.tsx
@@ -191,8 +191,6 @@ export default function TurntablesContent() {
           }}
         />
       </section>
-        </p>
-      </section>
 
       {/* Filters Section */}
       <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
